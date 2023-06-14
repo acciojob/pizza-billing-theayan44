87156b99bddeb8279015c022f84da6b10e858527
ayan <theayan44@gmail.com>
2023-06-15 00:22:07 +0530

    master

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,13 +3,15 @@ package com.driver;
 public class Main {
   public static void main(String[] args) {
     DeluxePizza dp = new DeluxePizza(true);
-    System.out.println(dp.getPrice());
+//    System.out.println(dp.getPrice());
+    System.out.println(dp.getBill());
     dp.addTakeaway();
     dp.addExtraCheese();
     dp.addTakeaway();
     System.out.println(dp.getBill());
 
     Pizza p = new Pizza(true);
+    System.out.println(p.getBill());
     p.addExtraCheese();
     p.addExtraToppings();
     p.addTakeaway();
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -8,6 +8,7 @@ public class Pizza {
     private boolean cheeseAdded = false;
     private boolean toppingAdded = false;
     private boolean takeAway = false;
+    private boolean billDone = false;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
@@ -71,7 +72,11 @@ public class Pizza {
     }
 
     public String getBill(){
-        bill += "Total Price: " + price +"\n";
-        return this.bill;
+        if(!billDone){
+            bill += "Total Price: " + price +"\n";
+            billDone = true;
+            return this.bill;
+        }
+        return "";
     }
 }
