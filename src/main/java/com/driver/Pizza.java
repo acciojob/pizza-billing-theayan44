eab2f347673980d6e4aa8f494cca768883a86bf9
ayan <theayan44@gmail.com>
2023-06-14 20:45:27 +0530

    master

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,30 +5,66 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private boolean cheeseAdded = false;
+    private boolean toppingAdded = false;
+    private boolean takeAway = false;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+        if(isVeg)
+            setPrice(300);
+        else
+            setPrice(400);
         // your code goes here
     }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+//    public Boolean getVeg() {
+//        return isVeg;
+//    }
+//
+//    public void setVeg(Boolean veg) {
+//        isVeg = veg;
+//    }
+
+//    public void setBill(String bill) {
+//        this.bill = bill;
+//    }
 
     public int getPrice(){
         return this.price;
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        if(!cheeseAdded){
+            price += 80;
+            cheeseAdded = true;
+        }
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        addExtraCheese();
+        if(!toppingAdded){
+            if(isVeg)
+                price += 70;
+            else
+                price += 120;
+            toppingAdded = true;
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        if(!takeAway){
+            price += 20;
+            takeAway = true;
+        }
     }
 
     public String getBill(){
-        // your code goes here
+        bill = "Total Price: " + price;
         return this.bill;
     }
 }
